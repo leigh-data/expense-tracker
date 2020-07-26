81341581e9e4cd628f3c831f44d5cc7253a0def6
Leigh Michael Forrest <leigh.m.data@gmail.com>
2020-07-25 22:42:53 -0400

    Updates

---
+++ precache-manifest.a5c81bd96f2433b968ef92db4b26617b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0331469739ab0a663caa93c8e577a48",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "14fa8629e3823c33e0dd",
+    "url": "/expense-tracker/static/css/main.caf1db2f.chunk.css"
+  },
+  {
+    "revision": "a3886eb48eeb822dea54",
+    "url": "/expense-tracker/static/js/2.abb637fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.abb637fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14fa8629e3823c33e0dd",
+    "url": "/expense-tracker/static/js/main.c964edf9.chunk.js"
+  },
+  {
+    "revision": "7284d3afe7f27382a2ae",
+    "url": "/expense-tracker/static/js/runtime-main.ace402c5.js"
+  }
+]);
